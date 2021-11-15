341e8e4ca11210ffbcf7674b2d9d992e0de9678a
Joshua Bates AND <joshua.bates@and.digital>
2021-11-15 16:53:58 +0000

    feat: initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders header", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Header/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders body", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Body/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders footer", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Footer/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+            <h1>Header</h1>
+      </header>
+      <p className="app-body">Body</p>
+      <footer className="app-footer">Footer</footer>
+    </div>
+  );
+}
+
+export default App;
